8f365c7ed728bd066489288ae76aaa24758d70f2
ankitkumarsamota121 <ankitkumarsamota121@gmail.com>
2020-02-09 04:50:41 +0530

    First prototype of the app is ready

--- web_app/apps/customer_dashboard.py
+++ web_app/apps/customer_dashboard.py
@@ -10,6 +10,13 @@ import pandas as pd
 import numpy as np
 from random import random
 
+COLORS = {
+    'background': '#28283c',
+    'text': '#77d1d6',
+}
+
+customer_id = 1
+
 
 def get_three_values():
     a = np.random.randint(100, 500)
@@ -21,7 +28,6 @@ def get_three_values():
 def get_extended_values(value):
     a = np.random.randint(100, 500)
     b = np.random.randint(100, 500)
-    c = np.random.randint(100, 500)
     return [a, b, value]
 
 
@@ -30,118 +36,155 @@ def get_random_segment():
     return segments[np.random.randint(0, 5)]
 
 
-customer_id = np.random.randint(0, 99)
+def generate_arpu_graph(customer):
+    return {
+        'data': [
+            {
+                'x': ['June', 'July', 'August'],
+                'y': get_extended_values(df.iloc[customer]['ARPU']),
+                'type': 'bar'
+            },
+        ],
+        'layout': {
+            'title': 'Average Revenue per User',
+            'showlegend': False,
+            'colorscale': 'balance',
+            'legend': {
+                'x': 0,
+                'y': 1.0
+            },
+            'plot_bgcolor': COLORS['background'],
+            'paper_bgcolor': COLORS['background'],
+            'font': {
+                'color': COLORS['text']
+            },
+        }
+    }
+
+
+def generate_service_usage_graph(customer):
+    return {
+        'data': [
+            {
+                'x': ['June', 'July', 'August'],
+                'y': get_extended_values(df.iloc[customer]['Internet Usage']),
+                'name': 'Internet Usage',
+                'marker': {
+                    'color': 'rgb(55, 83, 109)'
+                }
+            },
+            {
+                'x': ['June', 'July', 'August'],
+                'y': get_extended_values(df.iloc[customer]['Voice Usage']),
+                'name': 'Voice Usage',
+                'marker': {
+                    'color': 'rgb(26, 118, 255)'
+                }
+            },
+            {
+                'x': ['June', 'July', 'August'],
+                'y': get_extended_values(df.iloc[customer]['SMS Usage']),
+                'name': 'SMS Usage',
+                'marker': {
+                    'color': 'rgb(26, 200, 55)'
+                }
+            }
+        ],
+        'layout': {
+            'title': 'Service Usage by the User',
+            'showlegend': False,
+            'colorscale': 'balance',
+            'legend': {
+                'x': 0,
+                'y': 1.0
+            },
+            'plot_bgcolor': COLORS['background'],
+            'paper_bgcolor': COLORS['background'],
+            'font': {
+                'color': COLORS['text']
+            }
+        }
+    }
+
+
+def generate_churn_probability(prob):
+    random_prob = np.random.randint(0, 100)
+    return html.Div(
+        id='churn-probability',
+        children=str(random_prob) + "%"
+    )
+
 
 df = pd.read_csv(r"D:\Events\VIL Codefest\CustomDash\web_app\appdata\sample_plotting.csv")
 
 layout = html.Div([
     html.Div([
         html.Div([
+            html.H3(
+                html.Strong(
+                    '''
+                    CUSTOMDASH
+                    ''', id='customdash'),
+            ),
             html.Div([
-                dcc.Markdown('''
-                    **Customer ID: **
-                '''),
-                html.Div(id='display-customer-id'),
-            ], className='col'),
-            html.Div([
-                html.H3(
-                    dcc.Markdown('''
-                        **CUSTOMDASH**
-                    '''),
+                dcc.Input(
+                    id='customer-id',
+                    type='number',
+                    placeholder='Enter Customer ID...',
+                    value=1,
+                    className='eight columns'
                 ),
-                dcc.Input(id='customer-id', type='number', placeholder='Enter Customer ID...', value=1),
-                html.Button('Submit', id='submit-button'),
-            ], 'col')
-        ], className='row'),
-    ], className='container-fluid'),
+                html.Button('Submit', id='submit-button', className='four columns'),
+            ], className='container', style={'width': '100%'})
+        ], className='six columns'),
+
+        html.Div([
+            html.P([
+                html.Strong('Customer ID: '),
+                html.Strong('1', id='display-customer-id'),
+            ], className='text-customer-id'),
+        ], className='six columns container', id='customer-info')
+    ], className='container'),
+
+    html.Div([
+        html.Div([
+            dcc.Graph(
+                id='arpu-graph',
+                style={'width': '100%'},
+                figure=generate_arpu_graph(customer_id))
+        ], className='six columns', style={'width': '48%'}),
+
+        html.Div([
+            dcc.Graph(
+                id='service-usage-graph',
+                figure=generate_service_usage_graph(customer_id)),
+        ], className='six columns'),
+
+    ], className='container'),
 
     html.Div([
         html.Div([
             html.Div([
-                dcc.Graph(
-                    id='arpu-graph',
-                    figure={
-                        'data': [
-                            {'x': ['June', 'July', 'August'], 'y': get_three_values(), 'type': 'bar'},
-                        ],
-                        'layout': {
-                            'title': 'Average Revenue per User'
-                        }
-                    },
-                ),
-
-            ], className='col'),
+                html.Strong('CHURN PROBABILITY', className='titles'),
+                generate_churn_probability(1)
+            ], className='six columns', id='display-customer-churn'),
             html.Div([
-                dcc.Graph(
-                    figure=dict(
-                        data=[
-                            dict(
-                                x=['June', 'July', 'August'],
-                                y=get_extended_values(df.iloc[customer_id]['Internet Usage']),
-                                name='Internet Usage',
-                                marker=dict(
-                                    color='rgb(55, 83, 109)'
-                                )
-                            ),
-                            dict(
-                                x=['June', 'July', 'August'],
-                                y=get_extended_values(df.iloc[customer_id]['Voice Usage']),
-                                name='Voice Usage',
-                                marker=dict(
-                                    color='rgb(26, 118, 255)'
-                                )
-                            ),
-                            dict(
-                                x=['June', 'July', 'August'],
-                                y=get_extended_values(df.iloc[customer_id]['SMS Usage']),
-                                name='SMS Usage',
-                                marker=dict(
-                                    color='rgb(26, 200, 55)'
-                                )
-                            )
-                        ],
-                        layout=dict(
-                            title='Service Usage by the User',
-                            showlegend=True,
-                            legend=dict(
-                                x=0,
-                                y=1.0
-                            ),
-                            # margin=dict(l=40, r=10, t=40, b=30)
-                        )
-                    ), id='service-usage-graph'
-                )
-            ], className='col'),
-        ], className='row no-gutters')
-    ], className='container-fluid'),
-
-    html.Div([
-        html.Div([
-            html.Div([
-                dcc.Markdown('''**Churn Probability: **'''),
-                html.Div([
-                    random(),
-                ], id='churn-probability'),
-            ], className='col'),
-            html.Div([
-                dcc.Markdown('''
-                    **Customer Segment: **
-                    '''),
+                html.Strong('CUSTOMER SEGMENT', className='titles'),
                 html.Div([
                     get_random_segment()
                 ], id='customer-segment')
-            ], className='col')
-        ], className='row')
-    ], className='container-fluid'),
+            ], className='six columns', id='display-customer-segment')
+        ], className='container'),
 
-    # dcc.Link('Go to App 2', href='/apps/overall_dashboard')
+        # dcc.Link('Go to App 2', href='/apps/overall_dashboard')
+    ], id='last-div')
 ])
 
 
 @app.callback(
     Output('display-customer-id', 'children'),
     [Input('submit-button', 'n_clicks')],
-    [State('customer-id', 'value')]
+    [State('customer-id', 'value')],
 )
 def update_customer_id(n_clicks, value):
     return value
@@ -153,44 +196,7 @@ def update_customer_id(n_clicks, value):
     [State('customer-id', 'value')]
 )
 def service_usage_graph(n_clicks, value):
-    return dict(
-        data=[
-            dict(
-                x=['June', 'July', 'August'],
-                y=get_extended_values(df.iloc[value]['Internet Usage']),
-                name='Internet Usage',
-                marker=dict(
-                    color='rgb(55, 83, 109)'
-                )
-            ),
-            dict(
-                x=['June', 'July', 'August'],
-                y=get_extended_values(df.iloc[value]['Voice Usage']),
-                name='Voice Usage',
-                marker=dict(
-                    color='rgb(26, 118, 255)'
-                )
-            ),
-            dict(
-                x=['June', 'July', 'August'],
-                y=get_extended_values(df.iloc[value]['SMS Usage']),
-                name='SMS Usage',
-                marker=dict(
-                    color='rgb(26, 200, 55)'
-                )
-            )
-        ],
-        layout=dict(
-            title='Service Usage by the User',
-            showlegend=True,
-            legend=dict(
-                x=0,
-                y=1.0
-            ),
-            transition={'duration': 500}
-            # margin=dict(l=40, r=10, t=40, b=30)
-        )
-    )
+    return generate_service_usage_graph(value)
 
 
 @app.callback(
@@ -199,14 +205,7 @@ def service_usage_graph(n_clicks, value):
     [State('customer-id', 'value')]
 )
 def update_arpu_graph(n_clicks, value):
-    return {
-        'data': [
-            {'x': ['June', 'July', 'August'], 'y': get_three_values(), 'type': 'bar'},
-        ],
-        'layout': {
-            'title': 'Average Revenue per User'
-        }
-    }
+    return generate_arpu_graph(value)
 
 
 @app.callback(
@@ -214,8 +213,8 @@ def update_arpu_graph(n_clicks, value):
     [Input('submit-button', 'n_clicks')],
     [State('customer-id', 'value')]
 )
-def update_arpu_graph(n_clicks, value):
-    return random()
+def update_churn_probability(n_clicks, value):
+    return str(np.random.randint(0, 100)) + "%"
 
 
 @app.callback(
@@ -223,6 +222,5 @@ def update_arpu_graph(n_clicks, value):
     [Input('submit-button', 'n_clicks')],
     [State('customer-id', 'value')]
 )
-def update_arpu_graph(n_clicks, value):
+def update_customer_segment(n_clicks, value):
     return get_random_segment()
-
